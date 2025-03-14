123aa6e57cb5a6f3f970c0459cf77a287b68542d
naresh263 <nareshdhumal263@gmail.com>
2025-03-14 18:43:29 +0530

    initial push

---
+++ project_data.js
@@ -0,0 +1,285 @@
+var project_data = {
+    'cs':{
+        "project_name":"Chartered Studies",
+        "logo":"project_thumb.png", //image will be 330*220
+        "url":"https://testseries.charteredstudies.in/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"Chartered Studies is an exam portal for CA aspirants. This portal provides students with both written and MCQ exams. Purchased batches contain papers that students solve and upload to the server. The CS team then evaluates the sheets, grades them, and uploads the checked sheets. This portal also offers additional features such as a discussion forum and quizzes with certificates. This application contains settings and options to show data, sections dynamically and enables disabled settings from admin side",
+        'screenshots':[
+            {"name":'Home page',"image":'homepage1.png'},
+            {"name":'Home page',"image":'homepage2.png'},
+            {"name":'List of all batches with filter',"image":'buytest.png'},
+            {"name":'List of all batches with filter',"image":'buytest2.png'},
+            {"name":'Registration Page',"image":'register1.png'},
+            {"name":'Registration Page',"image":'register2.png'},
+            {"name":'Login Page with password/OTP',"image":'login.png'},
+            {"name":'Student Dashboard',"image":'student_dash.png'},
+            {"name":'Details of exam',"image":'exam_details_page.png'},
+            {"name":'MCQ Exam Page',"image":'mcq_exam.png'},
+
+            {"name":'Admin Dashboard',"image":'admin_dash.png'},
+            {"name":'Admin Dashboard',"image":'admin_dash_2.png'},
+            {"name":'Student Sheets Listing',"image":'student_sheets.png'},
+            {"name":'Student Uploaded Sheet Details',"image":'uploaded_sheet_details.png'},
+            {"name":'Checked Sheet Upload Page',"image":'checked_sheet_upload_page.png'},
+            {"name":'Discussion Forum',"image":'discussion_forum.png'},
+           
+        ]
+    },
+    // 'CCP':{
+    //     "project_name":"Chartered Studies",
+    //     "logo":"project_thumb.png", //image will be 330*220
+    //     "url":"https://testseries.charteredstudies.in/",
+    //     "private_reason":'0',
+    //     "organization":"Parasight Solutions",
+    //     "short_description":"chartered studies is an exam portal for ca anspirants",
+    //     "description":"chartered studies is an exam portal for ca anspirants",
+    //     'screenshots':[
+    //         {"name":'name',"image":'image'},
+    //         {"name":'name',"image":'image'},
+    //         {"name":'name',"image":'image'},
+    //         {"name":'name',"image":'image'},
+    //         {"name":'name',"image":'image'},
+    //     ]
+    // },
+    'PEM':{
+        "project_name":"Marketing Expense Manager",
+        "logo":"exp_mng.png", //image will be 330*220
+        "url":"https://marketingbudgetsoftware.podareducation.org/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"Marketing Expense Manager is a tool for tracking advertising expenses",
+        "description":"Marketing Expense Manager is a tool for tracking money spend on different type of marketing and adventising, and this application is linked with lead square so that team can calculate costing per admission",
+        'screenshots':[
+            {"name":'Login Screen',"image":'login.png'},
+            {"name":'Dashboard',"image":'dash1.png'},
+            {"name":'Dashboard',"image":'dash2.png'},
+            {"name":'Expense List',"image":'expenselist.png'},
+            {"name":'Expense Form',"image":'expenseform.png'},
+            {"name":'Detailed Report',"image":'detail_report.png'},
+            {"name":'Summary Report',"image":'summary_report.png'},
+        ]
+    },
+    
+    'fpg':{
+        "project_name":"FL London",
+        "logo":"fpg-logo.png", //image will be 330*220
+        "url":"https://fieldsliving.co.uk/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"fields living is a UK based property dealers. ",
+        "description":"fields living is an UK based firm who helps customers to buy, sell and rent their property, this application made with  laravel and google map api for autocomplete adresses, find nearest transport locations, show the route from transport location to property, display data on maps",
+        'screenshots':[
+            {"name":'Landing Page',"image":'home1.png'},
+            {"name":'List Properties',"image":'properties_list.png'},
+            {"name":'List Properties',"image":'list-properties.png'},
+            {"name":'View Property Details',"image":'view-property1.png'},
+            {"name":'View Property Details- transport locations',"image":'view-property2.png'},
+            {"name":'Routes to the property',"image":'route-to_property.png'},
+            {"name":'Admin Login',"image":'admin-login.png'},
+            {"name":'Admin Dashboard',"image":'admin-dashboard.png'},
+            {"name":'Add Property Form',"image":'add-property-form.png'},
+            {"name":'Nearby transport location based on landmark',"image":'transport.png'},
+            {"name":'Import Properties from BLM file',"image":'import-blm.png'},
+            
+        ]
+    },
+    'kf-risk':{
+        "project_name":"Kingfields Risk Management",
+        "logo":"kingfields-logo.png", //image will be 330*220
+        "url":"https://webflix.uk/risk-management/login",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"KF Risk Management is a tool for solicitors to analyse risk in cases ",
+        "description":"This application contains different types of forms which helps to find the risk in each case. the forms are msq based as well as different types of input forms and all the questions and its type controlled by admin.",
+        'screenshots':[
+            {"name":'Login Screen solicitor',"image":'login.png'},
+            {"name":'Solicitor Dashboard',"image":'solic-dash.png'},
+            {"name":'MCQ Based Survey Form',"image":'mcq-form.png'},
+            {"name":'Signature Draw/Upload Area',"image":'Signature-drwapad.png'},
+            {"name":'Dynamic Form',"image":'dynamic.png'},
+            {"name":'Graphocal Report',"image":'graphical-report.png'},
+            {"name":'Questions',"image":'questions.png'},
+            {"name":'Admin Dashboard',"image":'graphical-report.png'},
+        ]
+    },
+    'saas-ccp':{
+        "project_name":"CCP",
+        "logo":"jmbaxi-logo.png", //image will be 330*220
+        "url":"https://customercare.jmbaxi.com/login",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"Customer Complaint Portal is created to solve issues of exporters",
+        "description":"Customer Complaint Portal is laravel based application to solve issues of exporters, recieved suggestions and feedback from customer to all ports of JMBaxi. customer resgister their complaint on portal and port will provide solution to the exportal it follows sequence of approvals to provide end solution.",
+        'screenshots':[
+            {"name":'Login Screen',"image":'login.png'},
+            {"name":'Customer Dashboard',"image":'cust-dash.png'},
+            {"name":'Create Dispute form',"image":'create-dispute.png'},
+            {"name":'Admin Dashboard',"image":'admin-dash.png'},
+            {"name":'View Dispute',"image":'view-dispute.png'},
+            {"name":'View Dispute - Timeline',"image":'view-dispute-timeline.png'},
+            {"name":'Dispute forward/Action page',"image":'action.png'},
+            {"name":'Chating window for dispute',"image":'chatwindow.png'},
+        ]
+    },
+
+    'nirvik':{
+        "project_name":"Nirvik grievance filing system",
+        "logo":"nirvik.png", //image will be 330*220
+        "url":"",
+        "private_reason":'Appliction link not Available',
+        "organization":"Phoenix infotech",
+        "short_description":"Nirvik GFS is a portal for bank users to solve issues of exporters",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+           
+        ]
+    },
+
+    'ecgc':{
+        "project_name":"ECGC Proposal Form",
+        "logo":"ecgc.png", //image will be 330*220
+        "url":"",
+        "private_reason":'Appliction link not Available',
+        "organization":"phoenix infotech",
+        "short_description":"this application created for digitized  the process of insurance forms",
+        "description":"",
+        'screenshots':[
+           
+        ]
+    },
+
+    'nsdl-e-approval':{
+        "project_name":"NSDL E-Approvals",
+        "logo":"nsdl.png", //image will be 330*220
+        "url":"https://customercare.jmbaxi.com/login",
+        "private_reason":'Private Application',
+        "organization":"NSDL",
+        "short_description":"NSDL E-Approvals digitized the approvals/permssion system ",
+        "description":"",
+        'screenshots':[
+            
+        ]
+    },
+
+
+    //Idea Portal
+    'idea-portal':{
+        "project_name":"Idea Portal",
+        "logo":"jmbaxi-logo.png", //image will be 330*220
+        "url":"",
+        "private_reason":'Application not hosted',
+        "organization":"Parasight Solutions",
+        "short_description":"Idea Portal created for share ideas & impleted them for organization ",
+        "description":"Idea Portal created for whole JMbaxi group and its port where all the employees can share there idea on portal then assessment team will review it and send for approval, same for the approval the team review and approve budget and implementation team will implement the idea and employee gets reward ",
+        'screenshots':[
+            {"name":'Login Screen',"image":'login.png'},
+            {"name":'Admin Dashboard',"image":'admin-dashboard.png'},
+            {"name":'Roles & permissions',"image":'roles.png'},
+            {"name":'User Dashboard',"image":'user-dash.png'},
+            {"name":'Create Idea Page',"image":'create-idea.png'},
+            {"name":'Ideas Listing',"image":'idea-list.png'},
+            {"name":'Assessment team view details page of idea',"image":'team-idea-view.png'},
+        ]
+    },
+    //birla Library
+    'birla-library':{
+        "project_name":"Birla Library",
+        "logo":"birla-lib.png", //image will be 330*220
+        "url":"http://library.bkbck.edu.in",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"BKBCL is library portal for college and students",
+        "description":"BKBCL portal is modular application to show college library and other edutional details as well as news and achievement. all sections and links are manage from cms",
+        'screenshots':[
+            {"name":'Home Page',"image":'home1.png'},
+            {"name":'Home Page',"image":'home2.png'},
+            {"name":'Home Page',"image":'home3.png'},
+            {"name":'Home Page',"image":'home4.png'},
+            {"name":'Home Page',"image":'home5.png'},
+            {"name":'Home Page',"image":'home6.png'},
+            {"name":'Home Page Footer',"image":'home-footer.png'},
+            {"name":'Inner Pages',"image":'pages.png'},
+            {"name":'Admin Dashboard',"image":'admin-dash.png'},
+            {"name":'Create Gallay Form',"image":'create-gallary.png'},
+            {"name":'Create CMS Page',"image":'create-cms.png'},
+            {"name":'Create News',"image":'create-news.png'},
+        ]
+    },
+    //b-there
+    'b-there':{
+        "project_name":"B-there Gifts",
+        "logo":"b-there-logo.png", //image will be 330*220
+        "url":"https://rjnmejkj.a2hosted.com/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"B-there is a customisible gifts e-commerce portal",
+        "description":"B-there is a customisible gifts e-commerce portal. here customers can buy large variety of gifts such as photoframe, audio-photo frames, mugs, QR codes.",
+        'screenshots':[
+            {"name":'Landing Page',"image":'home.png'},
+            {"name":'Home Page',"image":'home1.png'},
+            {"name":'Home Page',"image":'home2.png'},
+            {"name":'Home Page',"image":'home3.png'},
+            {"name":'Products Listing',"image":'listing-page.png'},
+            {"name":'Login',"image":'customer-login.png'},
+            {"name":'Orders Listing Customer',"image":'cust-orders.png'},
+            {"name":'Upload Assets',"image":'upload-assets.png'},
+            {"name":'Admin Dashboard',"image":'admin-dash.png'},
+            {"name":'Admin View Order',"image":'view-order.png'},
+            {"name":'Admin View Order',"image":'view-order2.png'},
+            {"name":'Admin Orders Listing',"image":'orders-listing.png'},
+            
+        ]
+    },
+
+     //b-there
+     'cs-affil':{
+        "project_name":"CS Affiliation System",
+        "logo":"project_thumb.png", //image will be 330*220
+        "url":"https://testseries.charteredstudies.in/partner",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"CS Affilaite Portal designed for increase sales for batches and video courses",
+        "description":"CS Affilaite Portal is use for sell batches and video courses saparatly from the main portal, and earned commission from batches, partners comminicate with students and sell courses to them.",
+        'screenshots':[
+            {"name":'Admin Dashboard',"image":'admin-dashboard.png'},
+            {"name":'Partners Listing',"image":'partners.png'},
+            {"name":'Sales Data Listing',"image":'sales-data.png'},
+            {"name":'Set comission to partner based on subcategory',"image":'partner-commision.png'},
+            {"name":'Partner Login',"image":'partner-login.png'},
+            {"name":'Partner Dashboard',"image":'partner-dashboard.png'},
+            {"name":'Partner Dashboard',"image":'partner-dsh1.png'},
+            {"name":'Create Purchase - Batch Selection',"image":'create-purchase.png'},
+            {"name":'Create Purchase - Student Details',"image":'create-purchase-student-details.png'},
+            {"name":'Wallet ',"image":'wallet.png'},
+            
+        ]
+    },
+
+    // vani ERP
+    //Academy
+    'vani-erp':{
+        "project_name":"Vani ERP",
+        "logo":"vani-logo.png", //image will be 330*220
+        "url":"",
+        "private_reason":'Application is under Review',
+        "organization":"Parasight Solutions",
+        "short_description":"Vani ERP is designed for manage accounts of school",
+        "description":"Vani ERP is an ERP System designed for vani vidyalaya for manage thir accounts, this software contains credit-debit vouchers, petty cash, intersectional transaction , school fees refund, fees data fetch from api. this system also manage journal and ledger of records",
+        'screenshots':[
+            {"name":'Dashboard',"image":'dashboard.png'},
+            {"name":'Credit Vouchers Listing',"image":'vouchers.png'},
+            {"name":'Create Voucher',"image":'create-voucher.png'},
+            {"name":'Receipts Payments',"image":'receipts_payments.png'},
+            {"name":'Income Vouchers',"image":'income.png'},
+            {"name":'Intersectional Transaction',"image":'create-intersec-transaction.png'},
+            {"name":'Create Contra Entry',"image":'contra-entry.png'},
+            {"name":'Ledger',"image":'ledger.png'},
+            {"name":'Trail Balance',"image":'trail-balance.png'},
+        ]
+    },
+
+
+};
